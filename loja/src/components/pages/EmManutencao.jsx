ebde920def368ae7bef112eaf634a5127c0d973e
Alex12Rodrigues <alex.rodoliveira2@gmail.com>
2024-11-21 22:05:05 -0300

    Manutenção finalizada

--- loja/src/components/pages/EmManutencao.jsx
+++ loja/src/components/pages/EmManutencao.jsx
@@ -5,11 +5,20 @@ import style from "./EmManutencao.module.css";
 const EmManutencao = () => {
     return (
         <div className={style.container}>
-            <h1>Em Manutenção</h1>
-            <p>Estamos trabalhando para melhorar a sua experiência.</p>
-            <Link to="/" className={style.button}>
-                Voltar para Home
-            </Link>
+            <div className={style.content}>
+                <img
+                    src="/manutenção.jpg" 
+                    alt="Página em manutenção"
+                    className={style.image}
+                />
+                <h1 className={style.title}>Estamos em Manutenção</h1>
+                <p className={style.message}>
+                    Estamos ajustando os detalhes para oferecer uma experiência ainda melhor. Por favor, volte em breve!
+                </p>
+                <Link to="/" className={style.button}>
+                    Voltar para Home
+                </Link>
+            </div>
         </div>
     );
 };
